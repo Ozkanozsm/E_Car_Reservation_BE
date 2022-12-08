a9e87fe04ce60682ae836fe884b22cb3cb8ef611
Ozkan Ozesme <ozkan.ozesme@kirapratik.com>
2022-12-08 15:27:33 +0300

    add balance

--- src/datas/constants.ts
+++ src/datas/constants.ts
@@ -1,4 +1,4 @@
 export const userRegisterMessage = "register to e-car-reservation";
-export const web3Url = "http://192.168.0.12:7545";
+export const web3Url = "http://0.0.0.0:7545";
 export const stationRegisterMessage =
   "register to e-car-reservation as station";
---
+++ src/routes/wallet.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import Web3 from "web3";
+import { web3Url } from "../datas/constants";
+
+const walletRouter = express.Router();
+
+walletRouter.get("/", (req, res) => {
+  res.send("wallet router");
+});
+
+walletRouter.post("/balance", async (req, res) => {
+  const { address } = req.body;
+  try {
+    const web3 = new Web3(web3Url);
+    const balance = web3.utils.fromWei(
+      await web3.eth.getBalance(address),
+      "ether"
+    );
+    res.json({ balance });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+export default walletRouter;
